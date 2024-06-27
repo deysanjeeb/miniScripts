0ad20cdca52262bfd3919f79a2a42ea204a4bcbc
Sanjeeb Dey <sanjeebdey1@gmail.com>
2024-06-27 11:09:52 +0530

    find current market price

--- finance/nse_in/isin.py
+++ finance/nse_in/isin.py
@@ -15,21 +15,39 @@ def search_stock_by_isin(isin, date):
         print(dat)
         # Get the stock info
         info = stock.info
-        prices = yf.download(info['symbol'], start=dat, end=end)
+        # pprint(info)
+        try:
+            prices = yf.download(info['symbol'], start=dat, end=end)
 
-        # Check if we got valid data
-        if 'longName' in info:
-            return {
-                'name': info['longName'],
-                'symbol': info['symbol'],
-                'exchange': info['exchange'],
-                'Open': prices['Open'].iloc[0],
-                'High': prices['High'].iloc[0],
-                'Low': prices['Low'].iloc[0],
-                'Close': prices['Close'].iloc[0],
-            }
-        else:
-            return None
+        
+            if 'longName' in info:
+                return {
+                    'name': info['longName'],
+                    'symbol': info['symbol'],
+                    'exchange': info['exchange'],
+                    'Open': prices['Open'].iloc[0],
+                    'High': prices['High'].iloc[0],
+                    'Low': prices['Low'].iloc[0],
+                    'Close': prices['Close'].iloc[0],
+                    'currentMarketPrice': info['regularMarketOpen']
+                }
+            else:
+                return None
+        except:
+            if 'longName' in info:
+                return {
+                        'name': info['longName'],
+                        'symbol': info['symbol'],
+                        'exchange': info['exchange'],
+                        'Open': None,
+                        'High': None,
+                        'Low': None,
+                        'Close': None,
+                        'currentMarketPrice': info['regularMarketOpen']
+                    }
+            else:
+                return None
+
     except Exception as e:
         print(isin)
         print(f"An error occurred: {e}")
@@ -37,34 +55,51 @@ def search_stock_by_isin(isin, date):
 
 
 # Example usage
-
-df = pd.read_csv('pg8to12.csv')
-print(df['Tables ISIN'])
+filename = 'KPK158'
+df = pd.read_csv(f'{filename}.csv')
+print(df['ISINNO'])
 date = '2018-01-31'
 
 stock_results= {}
-for isin in df['Tables ISIN']:
-    result = search_stock_by_isin(isin, date)
+for isin in df['ISINNO']:
+    try:
+        result = search_stock_by_isin(isin, date)
+        
+    except:
+        stock = yf.Ticker(isin)
+        info = stock.info
+        result = {
+            'name': info['longName'],
+            'symbol': info['symbol'],
+            'exchange': info['exchange'],
+            'Open': None,
+            'High': None,
+            'Low': None,
+            'Close': None,
+            'currentMarketPrice': info['regularMarketOpen']
+        }
 
     if result:
         print(f"Stock Name: {result['name']}")
         print(f"Symbol: {result['symbol']}")
         print(f"Exchange: {result['exchange']}")
         print(f"Open: {result['Open']}")
+        print(f"current_price {result['currentMarketPrice']}")
         stock_results[isin] = result
+
     else:
         print("Stock not found or there was an error in the search.")
         stock_results[isin] = None
 
 pprint(stock_results)
-with open(f'stock_results_{date}.csv', mode='w', newline='') as file:
+with open(f'{filename}_stock_results_{date}.csv', mode='w', newline='') as file:
     writer = csv.writer(file)
     # Write the header
-    writer.writerow(['ISIN', 'Stock Name', 'Symbol', 'Exchange', 'Open', 'High', 'Low', 'Close'])
+    writer.writerow(['ISIN', 'Stock Name', 'Symbol', 'Exchange', 'Open', 'High', 'Low', 'Close', 'currentMarketPrice'])
     
     # Write the data
     for isin, info in stock_results.items():
         if info:  # If there is a result for the ISIN
-            writer.writerow([isin, info['name'], info['symbol'], info['exchange'], info['Open'], info['High'], info['Low'], info['Close']])
+            writer.writerow([isin, info['name'], info['symbol'], info['exchange'], info['Open'], info['High'], info['Low'], info['Close'], info['currentMarketPrice']])
         else:  # If the search was unsuccessful or there was an error
             writer.writerow([isin, 'Not Found', 'Not Found', 'Not Found'])
